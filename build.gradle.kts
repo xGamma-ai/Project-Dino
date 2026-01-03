31986ada04f431f5e03db9126518ed5599ed1629
alwin t varghese <alwintv5018@gmail.com>
2026-01-03 19:47:55 +0530

    feat: cocoapods-integration in progress.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.ktor) apply false
+    alias(libs.plugins.kotlinCocoapods) apply false
 }
